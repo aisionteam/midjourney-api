bf69bfa54bdbcdf6f16e24f4af478afc3655abcd
Ali Reza Riahi <ali.r.riahi@gmail.com>
2023-07-12 14:10:47 +0000

    redis connection

--- src/configs/redis.config.ts
+++ src/configs/redis.config.ts
@@ -1,14 +1,30 @@
 import configs from "../configs/env.configs";
 import redis, { createClient, RedisClientOptions } from 'redis';
 
-export const client = createClient({
-    host: configs.redis.redisUri,
-} as RedisClientOptions);
+// export const client = createClient({
+//     host: configs.redis.redisUri,
+// } as RedisClientOptions);
 
-client.on('connect', () => {
-    console.log('Connected to Redis');
-});
+// client.on('connect', () => {
+//     console.log('Connected to Redis');
+// });
 
-client.on('error', (error) => {
-    console.error('Redis error:', error);
-});
+// client.on('error', (error) => {
+//     console.error('Redis error:', error);
+// });
+
+export const client = async () => {
+    const redisClient = createClient({
+        host: configs.redis.redisUri,
+    } as RedisClientOptions);
+
+    redisClient.on('error', (error) => {
+        console.error('Redis error:', error);
+    });
+
+    redisClient.on('connect', () => {
+        console.log('Connected to Redis');
+    })
+    await redisClient.connect();
+    return redisClient;
+}
